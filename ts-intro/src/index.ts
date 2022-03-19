fc3887e9996bc93b9f49eac556b668fae3c973ed
SandovalJr <edubizzle7@gmail.com>
2022-03-18 20:14:30 -0600

    objects and arrays

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -8,7 +8,28 @@ hp = "siete siete";
 
 let vivo: boolean = true;
 
+//objetos y arrays
+let habilidades: string[] = ["elias", "matias", "daniel", "emmanuel"];
 
+habilidades.push("juan");
+console.log(habilidades);
+
+interface personajesi {
+  nombre: string;
+  hp: number;
+  habilidades: string[];
+  pueblonatal: string;
+}
+
+const personaje: any = {
+  nombre: "elias",
+  hp: 100,
+  habilidades: ["fuego", "agua"],
+  pueblonatal: "aguascalientees",
+};
+
+//imprimir una tabla
+console.table(personaje);
 
 /*
     ===== Código de TypeScript =====
