101f1df42f07f1f81e7ca7dfc44f7491949a3f09
SandovalJr <edubizzle7@gmail.com>
2022-03-18 20:04:24 -0600

    facil la intro ts

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -3,6 +3,12 @@ console.log("Hola Mundo, que hacen!");
 let nombre: string = "elias";
 const apellido = "sandoval";
 
+let hp: number | string = 77;
+hp = "siete siete";
+
+let vivo: boolean = true;
+
+
 
 /*
     ===== Código de TypeScript =====
