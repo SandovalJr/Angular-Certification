068c9ee868a96dcd709c30801b81ff3dc2b7a670
SandovalJr <edubizzle7@gmail.com>
2022-03-18 20:21:12 -0600

    dis

---
+++ ts-intro/src/ejercicios/01-tiposbasico.ts
@@ -0,0 +1,15 @@
+console.log("Hola Mundo, que hacen!");
+
+let nombre: string = "elias";
+const apellido = "sandoval";
+
+let hp: number | string = 77;
+hp = "siete siete";
+
+let vivo: boolean = true;
+
+//objetos y arrays
+let habilidades: string[] = ["elias", "matias", "daniel", "emmanuel"];
+
+habilidades.push("juan");
+console.log(habilidades);
---
+++ ts-intro/src/ejercicios/02-arr-obj.interface.ts
@@ -0,0 +1,23 @@
+
+
+interface personajesi {
+    nombre: string;
+    hp: number;
+    habilidades: string[];
+    pueblonatal: string;
+  }
+  
+  const personaje: any = {
+    nombre: "elias",
+    hp: 100,
+    habilidades: ["fuego", "agua"],
+    pueblonatal: "aguascalientees",
+  };
+  
+  //imprimir una tabla
+  console.table(personaje);
+  
+  /*
+      ===== Código de TypeScript =====
+  */
+  
