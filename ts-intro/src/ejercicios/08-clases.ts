419a42f8efd928ad0d9a5c645c9980d14e574b86
SandovalJr <edubizzle7@gmail.com>
2022-03-23 20:12:16 -0600

    clases

---
+++ ts-intro/src/ejercicios/08-clases.ts
@@ -0,0 +1,27 @@
+class personaNormal {
+    constructor(public nombre: string, public direccion: string) {
+      this.nombre = nombre;
+      this.direccion = direccion;
+    }
+  }
+  
+  class Heroe extends personaNormal {
+    //   alterEgo: string;
+    //   edad: number;
+    //   nombreReal: number;
+  
+    // es una funcion que se llama cuando hago referencia a la clase
+    constructor(
+      public alterEgo: string,
+      public edad: number,
+      public nombreReal: string
+    ) {
+      //    console.log('hola');
+      super(nombreReal,'New York USA');
+    }
+  }
+  
+  const spiderman = new Heroe("spiderman", 35, "Tom Holland");
+  
+  console.log(spiderman);
+  
