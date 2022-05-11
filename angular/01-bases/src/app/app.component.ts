9228b281f3983bbae46adb51a40a4d5edf8151d1
SandovalJr <edubizzle7@gmail.com>
2022-05-11 10:52:14 -0500

    easy metodos y clases

--- angular/01-bases/src/app/app.component.ts
+++ angular/01-bases/src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'bases';
+  public titulo: string = 'Contador App';
+  public contador: number = 0;
+
+  acumular(valor: number) {
+    this.contador += valor;
+  }
+  // sumar() {
+  //   this.contador += 1;
+  // }
+
+  // restar() {
+  //   this.contador -= 1;
+  // }
 }
