1e29696eba3f78588da0ff3a959f2f39281a719b
SandovalJr <edubizzle7@gmail.com>
2022-05-30 14:18:52 -0500

    menu

--- angular/04-pipes-app/src/app/app.module.ts
+++ angular/04-pipes-app/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+
+// Modulo personalizado
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
+import { VentasModule } from './ventas/ventas.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PrimeNgModule, SharedModule,VentasModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
