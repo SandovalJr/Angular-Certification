a13b3f071f325015aededf6cbbf8a5ec3666f11f
SandovalJr <edubizzle7@gmail.com>
2022-05-30 20:31:44 -0500

    prime flex

--- angular/04-pipes-app/src/app/ventas/ventas.module.ts
+++ angular/04-pipes-app/src/app/ventas/ventas.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+
+
+
+
+import { BasicosComponent } from './pages/basicos/basicos.component';
 import { NumerosComponent } from './pages/numeros/numeros.component';
 import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
-import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
 
 @NgModule({
@@ -18,6 +23,6 @@ import { OrdenarComponent } from './pages/ordenar/ordenar.component';
     BasicosComponent,
     OrdenarComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, PrimeNgModule],
 })
 export class VentasModule {}
