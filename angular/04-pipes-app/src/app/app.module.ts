9c24b65e5d49782d0906360de8113ba0d7863e02
SandovalJr <edubizzle7@gmail.com>
2022-06-01 10:44:38 -0500

    pipes comunes y numeros

--- angular/04-pipes-app/src/app/app.module.ts
+++ angular/04-pipes-app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,10 +8,17 @@ import { AppRouterModule } from './routes/app-router.module';
 import { SharedModule } from './shared/shared.module';
 import { VentasModule } from './ventas/ventas.module';
 
+//Cambiar idioma de manera global para los pipes
+import '@angular/common/locales/global/es';
+import '@angular/common/locales/global/fr';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRouterModule, SharedModule, VentasModule],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr' },
+    { provide: LOCALE_ID, useValue: 'es' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- angular/04-pipes-app/src/app/ventas/pages/numeros/numeros.component.ts
+++ angular/04-pipes-app/src/app/ventas/pages/numeros/numeros.component.ts
@@ -3,14 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NumerosComponent implements OnInit {
+  ventasNetas: number = 2654858.5454;
+  porcentaje: number = 0.5756;
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
