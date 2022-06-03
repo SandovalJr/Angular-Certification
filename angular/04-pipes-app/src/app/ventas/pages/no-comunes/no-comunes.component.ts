6e84a10b67255707d4043ab8b944f6438991c9c0
SandovalJr <edubizzle7@gmail.com>
2022-06-02 19:06:24 -0500

    keyvalue pipe

--- angular/04-pipes-app/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ angular/04-pipes-app/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -3,14 +3,55 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NoComunesComponent implements OnInit {
+  // i18nSelect
+  nombre: string = 'Fernando';
+  genero: string = 'M';
 
-  constructor() { }
+  ivitacionMapa = {
+    F: 'invitarla',
+    M: 'invitarlo',
+  };
 
-  ngOnInit(): void {
+  // i18nPlural
+  clientes: string[] = ['Maria', 'elias', 'juan', 'chuy', 'fatima'];
+  clienetsMapa = {
+    '=0': 'no tenemos clientes esperando',
+    '=1': 'tenemos 1 cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    '=3': 'tenemos 3 clientes esperando',
+    other: 'tenemos # clientes esperando',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  borrarCliente() {
+    // console.log(this.clientes);
+    this.clientes.splice(0, 1);
+    // console.log(this.clientes);
   }
 
+  cambiarPersona() {
+    this.nombre = 'Melissa';
+    this.genero = 'F';
+  }
+
+
+  // KeyValue Pipe
+
+persona = {
+  nombre:'Elias',
+  edad:22,
+  direccion:'Ottawa, Canada'
+}
+
+
+
+
+
+  
 }
