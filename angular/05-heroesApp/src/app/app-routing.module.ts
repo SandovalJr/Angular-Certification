90e5474c31bdd6fefb2fe399c942f91a2b7f5e86
SandovalJr <edubizzle7@gmail.com>
2022-06-08 15:31:08 -0500

    children routes

---
+++ angular/05-heroesApp/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    // importar rutas hijas 
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+
+
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
