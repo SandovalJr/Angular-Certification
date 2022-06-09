3cb0934dceb4ff2fae6177420de5c0467f1de64a
SandovalJr <edubizzle7@gmail.com>
2022-06-09 13:18:22 -0500

    cards

--- angular/05-heroes-app/src/app/material/material.module.ts
+++ angular/05-heroes-app/src/app/material/material.module.ts
@@ -5,9 +5,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
-  exports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule]
+  exports: [MatSidenavModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule, MatIconModule, MatListModule]
 })
 export class MaterialModule { }
