12c9e855b472c1ce5639e9b3b858c6a7534e1272
SandovalJr <edubizzle7@gmail.com>
2022-06-21 19:32:42 -0500

    guard de auth

--- angular/05-heroes-app/src/app/app-routing.module.ts
+++ angular/05-heroes-app/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/guards/authgard.guard';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 
@@ -9,10 +10,12 @@ const routes: Routes = [
     path: 'auth',
     // importar rutas hijas para lazy load
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+
   },
   {
     path: 'heroes',
-    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule),
+    canLoad: [AuthGuard]
   },
   {
     path: '404',
